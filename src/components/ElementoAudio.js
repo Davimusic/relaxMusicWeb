5e73223067690dfbc1c191004a3f62e672f11fee
webnocode <gdfajardo6@soy.sena.edu.co>
2023-08-22 19:13:00 -0500

    22-2

--- src/components/ElementoAudio.js
+++ src/components/ElementoAudio.js
@@ -9,13 +9,15 @@ export function ElementoAudio(){
     return(
             <div id='contenedorAudios' style={{ width: "100%", height: '83vh', overflow: 'auto', scrollBehavior: 'smooth'}} className='scrollVertical color1 colorLetra1' >
                 {arreAudiosPadre.map((item, index) => (
-                    <div id={`secAudio${index}`} onClick={() => usarAudio(index, index)} key={index} style={{ width: "100%", height: '10vh', borderRadius: '0.5em', padding: '10px', display: "flex", marginBottom: '10px' }} className='efectoFondoTransparente'>
-                        <Imagenes link={item.imagenAudio} style={styleImages} className={'efectoGirar'}/>
-                        <div style={{ marginLeft: '20px', height: '10.5vh', width: '90vw', display: 'flex' }} className='scrollVertical'>
-                            <h3>{item.titulo}</h3>
-                            <h4>{item.contenido}</h4>
-                        </div>
-                        <Imagenes  onClick={() => alert('corazon')}   id={`corazon${index}`}  className={'efectoFondoColor'} style={styleImages} link='https://res.cloudinary.com/dplncudbq/image/upload/v1692318586/mias/corazon_ccetxa.png' />        
+                    <div id={`secAudio${index}`} onClick={() => usarAudio(index, index)} key={index} style={{ width: "100%", height: '10vh', borderRadius: '0.5em', padding: '15px', marginBottom: '2vh', display: "flex"}} className='efectoFondoTransparente'>
+                        
+                            <Imagenes link={item.imagenAudio} style={styleImages} className={'efectoGirar'}/>
+                            <div style={{ marginLeft: '20px', height: '9vh', width: '90vw', display: 'flex' }} className='scrollVertical'>
+                                <h3>{item.titulo}</h3>
+                                <h4>{item.contenido}</h4>
+                            </div>
+                            <Imagenes  onClick={() => alert('corazon')}   id={`corazon${index}`}  className={'efectoFondoColor'} style={styleImages} link='https://res.cloudinary.com/dplncudbq/image/upload/v1692318586/mias/corazon_ccetxa.png' />  
+                        
                     </div>
                 ))}
             </div>

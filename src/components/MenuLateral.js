b0e1bd1a9ca9a0565ca4e80c52730fcc5dfd3e2c
webnocode <gdfajardo6@soy.sena.edu.co>
2023-08-25 12:34:51 -0500

    25-2

--- src/components/MenuLateral.js
+++ src/components/MenuLateral.js
@@ -10,31 +10,31 @@ const menu = ({id}) => {
     
     const styleLi = {display: 'flex', marginRight: '4vh', marginBottom: '2vh'}
     const styleA = {marginRight: '5px',  marginRight: 'auto'}
-    const styleIma = {width: '6vh', height: '6vh',  marginLeft: 'auto'}
+    const styleIma = {width: '6vh', height: '6vh',  marginRight: '2vw'}
 
     return (
         <nav className='displayMenu color2 colorLetra1'>  
                 <ul className="container">
                     <li style={styleLi} className="espacioEquilatero">
-                        <a style={styleA} href="#"> Inicio</a>
                         <Imagenes style={styleIma} link='https://res.cloudinary.com/dplncudbq/image/upload/v1692318586/mias/casa_qqdrbh.png'/>
+                        <a style={styleA} href="#"> Inicio</a>
                     </li>
                     <li style={styleLi} className="espacioEquilatero">
-                        <a style={styleA} href="#"> Buscar</a>
                         <Imagenes style={styleIma} link='https://res.cloudinary.com/dplncudbq/image/upload/v1692318586/mias/lupa_dyyko5.png'/>
+                        <a style={styleA} href="#"> Buscar</a>
                     </li>
                     <div className="linea-horizontal" style={{marginTop: '3vh', marginBottom: '3vh'}}></div>
                     <li style={styleLi} className="espacioEquilatero">
-                        <a style={styleA} href="#"> Crear nueva lista</a>
                         <Imagenes style={styleIma} link='https://res.cloudinary.com/dplncudbq/image/upload/v1692318587/mias/agregar_elne1c.png'/>
+                        <a style={styleA} href="#"> Crear nueva lista</a>
                     </li>
                     <li style={styleLi} className="espacioEquilatero">
+                        <Imagenes style={styleIma} link='https://res.cloudinary.com/dplncudbq/image/upload/v1692318586/mias/corazon_ccetxa.png'/>
                         <a style={styleA} href="#"> Mis favoritos</a>
-                        <Imagenes style={styleIma} link='https://res.cloudinary.com/dplncudbq/image/upload/v1692318586/mias/corazon_ccetxa.png'/>
                     </li>
                     <li style={styleLi} className="espacioEquilatero">
-                        <a style={styleA} href="#"> Mis mas reporducidos</a>
                         <Imagenes style={styleIma} link='https://res.cloudinary.com/dplncudbq/image/upload/v1692318586/mias/corazon_ccetxa.png'/>
+                        <a style={styleA} href="#"> Mis mas reporducidos</a>
                     </li>
                     <div className="linea-horizontal" style={{marginTop: '3vh', marginBottom: '3vh'}}></div>
                     <SwitchMode/>

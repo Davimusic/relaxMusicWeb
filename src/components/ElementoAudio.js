34707f1cbb6fb409a5635769eafb30351782e167
webnocode <gdfajardo6@soy.sena.edu.co>
2023-08-25 19:20:04 -0500

    25noche4

--- src/components/ElementoAudio.js
+++ src/components/ElementoAudio.js
@@ -6,7 +6,6 @@ import { accionesModal } from "@/funciones/AccionesModal";
 
 
 export function ElementoAudio() {
-    const styleImages = { height: '9vh', width: '9vh', marginRight: '10px' }
     const arreAudiosPadre = arrePadre()
     const [meGustas, setMeGustas] = useState(Array(arreAudiosPadre.length).fill(false));
 
@@ -20,15 +19,15 @@ export function ElementoAudio() {
     };
 
     return (
-        <div id='contenedorAudios' style={{ width: "100%", height: '83vh', overflow: 'auto', scrollBehavior: 'smooth'}} className='scrollVertical color1 colorLetra1'>
+        <div id='contenedorAudios' style={{ width: "100%", height: '83vh', paddingBottom: '7vh', overflow: 'auto', scrollBehavior: 'smooth'}} className='scrollVertical color1 colorLetra1'>
             {arreAudiosPadre.map((item, index) => (
                 <div id={`secAudio${index}`} onClick={() => usarAudio(index, index)} key={index} style={{ width: "100%", height: '10vh', borderRadius: '0.5em', padding: '0.5vh', marginBottom: '2vh', display: "flex" }} className='efectoFondoTransparente'>
-                    <Imagenes link={item.imagenAudio} style={styleImages} className={'efectoGirar'} onClick={(event) => accionesModal().abrirModal(event, item)}/>
+                    <Imagenes link={item.imagenAudio} style={{height: '9vh', width: '9vh', marginRight: '10px'}} className={'efectoGirar'} onClick={(event) => accionesModal().abrirModal(event, item)}/>
                     <div style={{ marginLeft: '20px', height: '9vh', width: '90vw', display: 'flex' }} className='scrollVertical'>
                         <h3>{item.titulo}</h3>
                         <h4>{item.contenido}</h4>
                     </div>
-                    <Imagenes id={`corazon${index}`} style={styleImages} onClick={(event) => accionMeGusta(event, index)} link={meGustas[index] ? 'https://res.cloudinary.com/dplncudbq/image/upload/v1692753447/mias/cora_l5a4yp.png' : 'https://res.cloudinary.com/dplncudbq/image/upload/v1692318586/mias/corazon_ccetxa.png'} />
+                    <Imagenes id={`corazon${index}`} style={{height: '5vh', width: '5vh', marginRight: '10px', marginTop: '10px'}} onClick={(event) => accionMeGusta(event, index)} link={meGustas[index] ? 'https://res.cloudinary.com/dplncudbq/image/upload/v1692753447/mias/cora_l5a4yp.png' : 'https://res.cloudinary.com/dplncudbq/image/upload/v1692318586/mias/corazon_ccetxa.png'} />
                 </div>
             ))}
         </div>

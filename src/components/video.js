8a7afe61ec638607db8f8143ff5885cdfd16e6b2
webnocode <gdfajardo6@soy.sena.edu.co>
2023-08-25 21:23:07 -0500

    25-noche7

--- src/components/video.js
+++ src/components/video.js
@@ -9,6 +9,19 @@ import  Modal  from '../components/Modal';
 import 'react-toastify/dist/ReactToastify.css';
 
 export function Video(){
+    // Verificamos si el navegador admite la propiedad 'screen.orientation' y el método 'lock'
+if (screen.orientation && screen.orientation.lock) {
+    // Intentamos bloquear la orientación en landscape
+    screen.orientation.lock('landscape')
+      .then(function() {
+        console.log('La orientación se ha bloqueado en landscape');
+      })
+      .catch(function(error) {
+        console.error('Error al intentar bloquear la orientación:', error);
+      });
+  } else {
+    console.warn('La API de orientación no es compatible en este navegador.');
+  }
     return(
         <div>
             <Modal/>

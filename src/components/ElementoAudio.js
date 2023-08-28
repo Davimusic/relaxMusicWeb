36659ea3e078c8bcbde6f6c328f9ac4d6e78eca3
webnocode <gdfajardo6@soy.sena.edu.co>
2023-08-28 12:57:13 -0500

    28-4

--- src/components/ElementoAudio.js
+++ src/components/ElementoAudio.js
@@ -48,27 +48,25 @@ export function ElementoAudio() {
     }, [dataLoaded]);
 
     return (
-        <div>
-            {/*!dataLoaded ? (
+        <div id='contenedorAudios' style={{ width: "100%", height: '83vh', paddingBottom: '7vh', overflow: 'auto', scrollBehavior: 'smooth'}} className='scrollVertical color1 colorLetra1'>
+            {!dataLoaded ? (
                 <div className="loading-container color1">
-                    <div className="loading"></div>
+                <div className="loading"></div>
                 </div>
-            ) : (*/}
-                <div id='contenedorAudios' style={{ width: "100%", height: '83vh', paddingBottom: '7vh', overflow: 'auto', scrollBehavior: 'smooth'}} className='scrollVertical color1 colorLetra1'>
-                    {arreAudiosPadre.map((item, index) => (
-                        <div id={`secAudio${index}`} onClick={() => usarAudio(index, index)} key={index} style={{ width: "100%", height: '10vh', borderRadius: '0.5em', padding: '0.5vh', marginBottom: '2vh', display: "flex" }} className='efectoFondoTransparente'>
-                            <Imagenes link={item.imagenAudio} style={{height: '9vh', width: '9vh', marginRight: '10px'}} className={'efectoGirar'} onClick={(event) => accionesModal().abrirModal(event, item)}/>
-                            <div style={{ marginLeft: '20px', height: '9vh', width: '90vw', display: 'flex' }} className='scrollVertical'>
-                                <h3>{item.titulo}</h3>
-                                <h4>{item.contenido}</h4>
-                            </div>
-                            <Imagenes id={`corazon${index}`} style={{height: '5vh', width: '5vh', marginRight: '10px', marginTop: '10px'}} onClick={(event) => accionMeGusta(event, index)} link={meGustas[index] ? 'https://res.cloudinary.com/dplncudbq/image/upload/v1692753447/mias/cora_l5a4yp.png' : 'https://res.cloudinary.com/dplncudbq/image/upload/v1692318586/mias/corazon_ccetxa.png'} />
+            ) : (
+                arreAudiosPadre.map((item, index) => (
+                    <div id={`secAudio${index}`} onClick={() => usarAudio(index, index)} key={index} style={{ width: "100%", height: '10vh', borderRadius: '0.5em', padding: '0.5vh', marginBottom: '2vh', display: "flex" }} className={`efectoFondoTransparente  ${dataLoaded ? 'aparecer' : ''}`} >
+                        <Imagenes link={item.imagenAudio} style={{height: '9vh', width: '9vh', marginRight: '10px'}} className={'efectoGirar'} onClick={(event) => accionesModal().abrirModal(event, item)}/>
+                        <div style={{ marginLeft: '20px', height: '9vh', width: '90vw', display: 'flex' }} className='scrollVertical'>
+                        <h3>{item.titulo}</h3>
+                        <h4>{item.contenido}</h4>
                         </div>
-                    ))}
-                </div>
-            {/*)*/}
+                        <Imagenes id={`corazon${index}`} style={{height: '5vh', width: '5vh', marginRight: '10px', marginTop: '10px'}} onClick={(event) => accionMeGusta(event, index)} link={meGustas[index] ? 'https://res.cloudinary.com/dplncudbq/image/upload/v1692753447/mias/cora_l5a4yp.png' : 'https://res.cloudinary.com/dplncudbq/image/upload/v1692318586/mias/corazon_ccetxa.png'} />
+                    </div>
+                ))
+            )}
         </div>
-    );
+        );
 }
 
 export default ElementoAudio;
